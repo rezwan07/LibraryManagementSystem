9a2047cff480a9dd9efd47724293451b857a50d3
rezwan07 <rez1.ahmed007@gmail.com>
2026-05-21 13:22:55 +0600

    added enums

---
+++ LibraryManagementSystem.Core/Enums/BookStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Core.Enums
+{
+    public enum BookStatus
+    {
+        Available = 1,
+        Borrowed = 2,
+        Lost = 3,
+        Damaged = 4,
+        UnderMaintenance = 5
+    }
+}
---
+++ LibraryManagementSystem.Core/Enums/BorrowStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Core.Enums
+{
+    public enum BorrowStatus
+    {
+        Borrowed =1,
+        Returned = 2,
+        Overdue = 3,
+        ReturnedLate = 4
+    }
+}
