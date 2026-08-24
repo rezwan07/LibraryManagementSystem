cc35f58299a79d227312905c95e905072208181f
Rezwan07 <rez1.ahmed007@gmail.com>
2026-08-24 15:57:32 +0600

    rename IBookRepository.cs file

---
+++ LibraryManagementSystem.Infrastructure/Data/LibraryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext()
+        {
+
+        }
+    }
+}
