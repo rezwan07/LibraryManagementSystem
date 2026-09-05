986a06c491a98f14dcabcfef362609ee3131d378
Rezwan07 <rez1.ahmed007@gmail.com>
2026-09-06 05:04:19 +0600

    UNIQUE CONSTRAINTS (Duplicate data prevent)
    library db context

--- LibraryManagementSystem.Infrastructure/Data/LibraryDbContext.cs
+++ LibraryManagementSystem.Infrastructure/Data/LibraryDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LibraryManagementSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,49 @@ namespace LibraryManagementSystem.Infrastructure.Data
 {
     public class LibraryDbContext : DbContext
     {
-        public LibraryDbContext()
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
         {
+                
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BorrowRecord> BorrowRecords { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BorrowRecord>()
+                .HasOne(br => br.User)
+                .WithMany(u => u.BorrowRecords)
+                .HasForeignKey(br => br.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<BorrowRecord>()
+                .HasOne(br => br.Book)
+                .WithMany(b => b.BorrowRecords)
+                .HasForeignKey(br => br.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique()
+                .HasDatabaseName("IX_Users_Email_Unique");
+
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.ISBN)
+                .IsUnique()
+                .HasDatabaseName("IX_Books_ISBN_Unique");
+
+
 
         }
+
+
+
+
+
     }
 }
