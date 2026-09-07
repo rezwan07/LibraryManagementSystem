b275de6d64c171180061c746dd50658339153c8d
Rezwan07 <rez1.ahmed007@gmail.com>
2026-09-08 04:41:59 +0600

    0.9: Generic Repository Implementation

---
+++ LibraryManagementSystem.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,123 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Core.Interfaces;
+using LibraryManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly LibraryDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(LibraryDbContext context)
+        {
+            _context= context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            if(typeof(BaseEntity).IsAssignableFrom(typeof(T)))
+            {
+                var parameter = Expression.Parameter(typeof(T), "e");
+                var property = Expression.Property(parameter, "IsDeleted");
+                var falseValue = Expression.Constant(false);
+                var condition = Expression.Equal(property, falseValue);
+                var lambda = Expression.Lambda<Func<T, bool>>(condition, parameter);
+
+            }
+
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if(entity != null)
+            {
+                var property = typeof(T).GetProperty("IsDeleted");
+                if(property != null)
+                {
+                    property.SetValue(entity, true);
+                    await UpdateAsync(entity);
+                }
+                else
+                {
+                    _dbSet.Remove(entity);
+                    await _context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task DeletePermanentAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if(entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> Existasync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync()
+        {
+            if(typeof(BaseEntity).IsAssignableFrom(typeof(T)))
+            {
+                var parameter = Expression.Parameter(typeof(T));
+                var property = Expression.Property(parameter, "IsDeleted");
+                var falseValue = Expression.Constant(false);
+                var condition = Expression.Equal(property, falseValue);
+                var lambda = Expression.Lambda<Func<T, bool>>(condition, parameter);
+                return await _dbSet.CountAsync(lambda);
+            }
+            return await _dbSet.CountAsync();
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.CountAsync(predicate);
+        }
+    }
+}
