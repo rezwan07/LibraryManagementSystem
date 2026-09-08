515de02feb5b48603112f7d6d35090b8c445ea9d
Rezwan07 <rez1.ahmed007@gmail.com>
2026-09-08 15:18:53 +0600

    Step 0.10: BookRepository Implementation

---
+++ LibraryManagementSystem.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,67 @@
+﻿using LibraryManagementSystem.Core.Entities;
+using LibraryManagementSystem.Core.Interfaces;
+using LibraryManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Infrastructure.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        public BookRepository(LibraryDbContext context) : base(context)
+        {
+                
+        }
+
+        public async Task<IEnumerable<Book>> GetAvailableBooksAsync()
+        {
+            return await _dbSet
+                .Where(b => b.AvailableCopies > 0 && !b.IsDeleted)
+                .OrderBy(b => b.Title)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Book>> GetBookAuthorAsync(string author)
+        {
+            return await _dbSet
+                .Where (b => b.Author.Contains(author) && !b.IsDeleted)
+                .OrderBy (b => b.Title)
+                .ToListAsync ();
+        }
+
+        public async Task<IEnumerable<Book>> GetBookByCategoryAsync(string category)
+        {
+            return await _dbSet
+                .Where(b => b.Category != null && b.Category.Contains(category) && !b.IsDeleted)
+                .OrderBy(b => b.Title)
+                .ToListAsync();  
+        }
+
+        public async Task<Book?> GetBookByISBNAsync(string isbn)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(b => b.ISBN == isbn && !b.IsDeleted);
+        }
+
+        public async Task UpdateAvailabilityAsync(int bookId, int changeAmount)
+        {
+            var book = await GetByIdAsync(bookId);
+            if (book != null)
+            {
+                book.AvailableCopies += changeAmount;
+
+                if(book.AvailableCopies < 0)
+                    book.AvailableCopies = 0;
+
+                if(book.AvailableCopies < book.TotalCopies)
+                    book.AvailableCopies = book.TotalCopies;
+
+                await UpdateAsync(book);
+            }
+        }
+    }
+}
